3dcffd77471a90359d10d8ef83e97ba79cfc9a65
Mario Germán Reyes C <mager19@gmail.com>
2018-10-30 20:51:27 -0500

    Base actualizada

--- footer.php
+++ footer.php
@@ -13,20 +13,26 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'urbanvalue' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'urbanvalue' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'urbanvalue' ), 'urbanvalue', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="site-footer" style="background: lightgray; padding: 2em 0;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="site-info">
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'urbanvalue' ) ); ?>">
+							<?php
+							/* translators: %s: CMS name, i.e. WordPress. */
+							printf( esc_html__( 'Proudly powered by %s', 'urbanvalue' ), 'WordPress' );
+							?>
+						</a>
+						<span class="sep"> | </span>
+							<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Theme: %1$s by %2$s.', 'urbanvalue' ), 'urbanvalue', '<a href="http://underscores.me/">Underscores.me</a>' );
+							?>
+					</div><!-- .site-info -->
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- index.php
+++ index.php
@@ -14,46 +14,48 @@
 
 get_header();
 ?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<?php
+				if ( have_posts() ) :
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+					if ( is_home() && ! is_front_page() ) :
+						?>
+						<header>
+							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						</header>
+						<?php
+					endif;
 
-		<?php
-		if ( have_posts() ) :
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-			if ( is_home() && ! is_front_page() ) :
+						/*
+						 * Include the Post-Type-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_type() );
+
+					endwhile;
+
+					the_posts_navigation();
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
 				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+		</div>
+	</div>
+</div>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
